01b2cb7adb95dcab9842baaf6335b9113dcf481a
jeromel05 <32449869+jeromel05@users.noreply.github.com>
2017-11-03 20:41:22 +0100

    final html documentation

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['getbuffercase',['getBufferCase',['../classNeuron.html#a2f57e35482d19aff53765942cc137166',1,'Neuron']]],
   ['getconnections',['getConnections',['../classNetwork.html#a7404c50fcb926a4b19aea2c0c7cfcce4',1,'Network']]],
   ['getindex',['getIndex',['../classNeuron.html#a00f64bd651ad2cb04cd7465472d725ad',1,'Neuron']]],
   ['getmbpotential',['getMbPotential',['../classNeuron.html#a7b0a9570b046fbb67e091d46104c6edc',1,'Neuron']]],
